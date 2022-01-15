e43e638dd29ad55b7cfb3bb6b8a52b15ab7b39ff
dogukan <dogukan.matul@gmail.com>
2022-01-15 08:15:51 +0300

    Enitityler oluşturuldu

---
+++ PersonalBlog.Core/Models/Concrete/Author.cs
@@ -0,0 +1,17 @@
+﻿using PersonalBlog.Core.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Core.Models
+{
+    public class Author:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string SocialLink { get; set; }
+        public virtual ICollection<Blog> Blogs { get; set; }
+    }
+}
---
+++ PersonalBlog.Core/Models/Concrete/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Core.Models
+{
+    public class Blog
+    {
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string İmage { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+
+    }
+}
