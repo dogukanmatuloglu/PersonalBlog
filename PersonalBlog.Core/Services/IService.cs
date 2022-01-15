459842936ec3ef7d66552a7c2665cfc73b38c4f5
dogukan <dogukan.matul@gmail.com>
2022-01-15 09:45:43 +0300

    IService Generic interfaci oluşturuldu

--- PersonalBlog.Core/Services/IService.cs
+++ PersonalBlog.Core/Services/IService.cs
@@ -5,17 +5,17 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PersonalBlog.Core.Repositories
+namespace PersonalBlog.Core.Services
 {
-    public interface IGenericRepository<T> where T : class
+    interface IService<T> where T:class
     {
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
 
         Task AddAsync(T entity);
 
-        void Update(T entity);
-        void Delete(T entity);
+        Task UpdateAsync(T entity);//burada EF Core SaveChangesAsync metodu kullanılacağı için dönüş tipinin void yerine Task olması gerekiyor.
+        Task DeleteAsync(T entity);//burada EF Core SaveChangesAsync metodu kullanılacağı için dönüş tipinin void yerine Task olması gerekiyor.
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
     }
