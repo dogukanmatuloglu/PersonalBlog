063226cf9e32e0f2d3a37701750fe80a26d364db
dogukan <dogukan.matul@gmail.com>
2022-01-15 09:35:14 +0300

    Generic repository Oluşturuldu

---
+++ PersonalBlog.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAll();
+
+        Task AddAsync(T entity);
+
+        void Update(T entity);
+        void Delete(T entity);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+    }
+}
